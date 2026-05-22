099e37e4b1a6e8059b5d86cd2122c69f67b47390
Otchangelo Diogo <otchangelodiogo04@gmail.com>
2026-05-22 11:05:44 +0100

    login

--- login.js
+++ login.js
@@ -16,3 +16,57 @@ signupLink.onclick = () => {
   signupBtn.click();
   return false;
 };
+
+const users=
+[
+  {
+    email: "JoaoPestana@gmail.com",
+    senha: "12345"
+  },
+  {
+    email: "ManoChaba@gmail.com",
+    senha: "54321"
+  },
+  {
+    email: "PaiNatal@gmail.com",
+    senha: "45123"
+  }
+];
+
+function login(){
+  const user=document.getElementById('emailtxt').value;
+  const psswrd=document.getElementById('passtxt').value;
+  const usuario=users.find(u=>(u.email==user)) && users.find(u=>(u.senha==psswrd));
+ 
+  if(usuario){
+    window.location.href = "https://agrolotus.vercel.app/";
+    alert('Login realizado com sucesso');
+}
+}
+
+function register(){
+  const new_email=document.getElementById('newEmail').value;
+  const new_pass=document.getElementById('newPass').value;
+  const verify=document.getElementById('verify').value;
+  const userExists=users.find(u=>(u.email==new_email));
+  if(new_pass!=verify){
+    alert('As senhas não coincidem');
+  }
+  else{
+  if(userExists){
+    alert('Já existe');
+    return;
+  }
+  else{
+    users.push({email: new_email, senha: new_pass});
+    window.location.href="https://agrolotus.vercel.app/";
+    alert('Usuário criado');
+    
+    
+    
+  }
+}
+}
+
+
+
